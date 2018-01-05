c991776d749b8d9a30e06551a0d0dc2d0f48fa40
Ben Boreham <benab001@googlemail.com>
2018-01-05 11:20:55 +0000

    Create welcomeMessage method

--- MyConnectFour.java
+++ MyConnectFour.java
@@ -16,13 +16,17 @@ public class MyConnectFour {
 		playGame();
 	}
 	
+	private void welcomeMesssage() {
+	   System.out.println("Welcome to Connect 4");
+	   System.out.println("There are 2 players red and yellow");
+	   System.out.println("Player 1 is Red, Player 2 is Yellow");
+	   System.out.println("To play the game type in the number of the column you want to drop your counter in");
+	   System.out.println("A player wins by connecting 4 counters in a row - vertically, horizontally or diagonally");
+	   System.out.println("");
+	}
+	
 	private void playGame(){
-		System.out.println("Welcome to Connect 4");
-		System.out.println("There are 2 players red and yellow");
-		System.out.println("Player 1 is Red, Player 2 is Yellow");
-		System.out.println("To play the game type in the number of the column you want to drop you counter in");
-		System.out.println("A player wins by connecting 4 counters in a row - vertically, horizontally or diagonally");
-		System.out.println("");
+	  
 		printBoard();
 		boolean win = false;
 		while(!win){
